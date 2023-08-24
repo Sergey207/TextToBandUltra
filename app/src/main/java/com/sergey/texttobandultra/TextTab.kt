b493ca682ce7cc4abc7c8f030ceca01a3ddebb31
Sergey <isergey20207@gmail.com>
2023-08-24 20:43:05 +0300

    Updated TextTab

--- app/src/main/java/com/sergey/texttobandultra/TextTab.kt
+++ app/src/main/java/com/sergey/texttobandultra/TextTab.kt
@@ -3,11 +3,29 @@ package com.sergey.texttobandultra
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import com.google.gson.Gson
+import java.io.File
+
+const val UTF_SYM = '\uFEFF'
+val tabsPath = File("")
 
 data class TextTab(
     var title: String,
-    var text: MutableState<String> = mutableStateOf("")
+    var text: MutableState<String> = mutableStateOf(""),
+    var toSave: MutableState<Boolean> = mutableStateOf(false)
 )
+
+fun MutableList<TextTab>.save() {
+    for (tab in this) {
+        tabsPath
+            .resolve("${tab.title}.txt")
+            .writeText("$UTF_SYM${tab.text}", Charsets.UTF_16LE)
+        tab.toSave.value = false
+    }
+    tabsPath.resolve("tabs.json").writeText(
+        Gson().toJson(this.map { "${it.title}.txt" })
+    )
+}
 
 val tabs = mutableStateListOf<TextTab>()
 val currentIndex = mutableStateOf(0)
